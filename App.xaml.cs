8d58aac22d012cd393d80c09c8f22ecd6b91227e
furaihan <28698437+furaihan@users.noreply.github.com>
2023-01-24 09:19:26 +0700

    remove unused global variable

--- App.xaml.cs
+++ App.xaml.cs
@@ -11,7 +11,6 @@ namespace KatalogFilm
     {
         public static TMDbClient Client = new TMDbClient("a39c8049ea4b22d58e5ed78f6f09e62b");
         public static AccountDetails? Account = null;
-        internal const string poppinFontPath = @"Font\Poppins-Regular.ttf";
         private void Application_Startup(object sender, StartupEventArgs e)
         {
 
